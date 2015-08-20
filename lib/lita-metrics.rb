1a95a8935e75a7f9fbd606050d4acc98146c43cf
Tristan Chong <tristanchong@gmail.com>
2015-08-20 14:32:56 -0700

    lib: Require logger and statsd

--- lib/lita-metrics.rb
+++ lib/lita-metrics.rb
@@ -1,12 +1,15 @@
-require "lita"
+require 'lita'
 
 Lita.load_locales Dir[File.expand_path(
-  File.join("..", "..", "locales", "*.yml"), __FILE__
+  File.join('..', '..', 'locales', '*.yml'), __FILE__
 )]
 
-require "lita/handlers/metrics"
+require 'logger'
+require 'statsd'
+
+require 'lita/handlers/metrics'
 
 Lita::Handlers::Metrics.template_root File.expand_path(
-  File.join("..", "..", "templates"),
+  File.join('..', '..', 'templates'),
  __FILE__
 )
